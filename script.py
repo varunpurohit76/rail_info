b638a55e18f948012d1d2f847591090873843a05
varunpurohit76 <varunpurohit76@gmail.com>
2016-07-01 01:17:51 +0530

    wrong train numbers and passive train handeling

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import collections
 import json
+import sys
 
 def timetable(soup):
     table =  soup.find(id='eta')
@@ -23,13 +24,28 @@ def status(soup):
     content = [" ".join(ele.text.split()) for ele in content]
     return content
 
+def is_data_available(soup):
+    table = soup.find(id = 'eta')
+    if table is None:
+        p_content = soup.find('p')
+        if 'Unavailable' in str(p_content):
+            print ('Information Unavailble. Please check the train number.')
+        else:
+            p_content = p_content.text.strip()
+            print ('Train not running!')
+            print (p_content)
+        sys.exit()
+
 def main():
-    payload = {'tno':'12952', 'date':'0'}
+    train_num = sys.argv[1]
+    payload = {'tno':train_num, 'date':'0'}
+#12483
     r = requests.get('http://spoturtrain.com/status.php', params = payload)
     train_info = collections.OrderedDict()
     train_info['url'] = r.url
     data = r.text
     soup = BeautifulSoup(data,"lxml")
+    is_data_available(soup)
     train_info['status'] = status(soup)
     train_info['timetable'] = timetable(soup)
     train_info = json.dumps(train_info)
